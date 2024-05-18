5d10503aef8b2a22c71be601f89430faa3dcedc7
SBC - CuongNQ1 <cuongnq1@bsscommerce.com>
2024-05-18 22:48:04 +0700

    cart screen and footer

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import LandingScreen from './screens/LandingScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import Footer from './components/Footer';
 function App() {
   return (
     <ChakraProvider>
@@ -14,8 +16,10 @@ function App() {
             <Route path='/' element={<LandingScreen />} />
             <Route path='/products' element={<ProductsScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart' element={<CartScreen />} />
           </Routes>
         </main>
+        <Footer />
       </Router>
     </ChakraProvider>
   );
